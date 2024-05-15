e3c984a72b5c0c1b7cc93af000192c9064fe8037
Nurzat <n_turganbek@kbtu.kz>
2024-05-15 21:02:41 +0600

    changes all

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,7 +5,7 @@ import { LoginService } from '../login.service';
 
 @Component({
   selector: 'app-home',
-  templateUrl: '/./home.component.html',
+  templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
